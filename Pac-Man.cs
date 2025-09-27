af422638c823dcfdbda19feaa0cedcbc02879eff
Sajjal Fatima <134789255+Sajjal-515@users.noreply.github.com>
2025-09-27 14:15:09 +0500

    Add files via upload

---
+++ Pac-Man.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main()
+    {
+        string line = "-------";
+        int length = line.Length;
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("\r" + new string(' ', i) + "C" + line.Substring(i + 1));
+            Thread.Sleep(200);
+            Console.Write("\r" + new string(' ', i + 1) + line.Substring(i + 1));
+            Thread.Sleep(200);
+        }
+        Console.Write("\r" + new string(' ', length) + "C");
+        Console.WriteLine("\n\n Game over!Pac-Man ate all the dots!");
+    }
+}
